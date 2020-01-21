4270ad39d1c4e4e46a6e3446ab41707a469ff7e5
Emma Sheridan <emmasheridan@Emmas-MacBook-Air.local>
2020-01-21 20:42:12 +0000

    updatedkeyCodeChallenge

--- Tuesday/keyCodeChallenge/app.js
+++ Tuesday/keyCodeChallenge/app.js
@@ -1,6 +1,13 @@
 function keycode(){
 // put inside an eventlistener 
     let key = event.keyCode;
-    console.log(key)
+    console.log(event.key)
+    console.log(event.code)
+    console.log(event.which)
+    document.getElementById("keycode").textContent = event.keyCode;
+    document.getElementById("eventKey").textContent = event.key;
+    document.getElementById("eventCode").textContent = event.code;
+    document.getElementById("eventWhich").textContent = event.which;
+
 }
 
