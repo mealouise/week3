9305d526c57e4dadd08b5b6f06febe7dffaf5f05
Emma Sheridan <emmasheridan@Emmas-MacBook-Air.local>
2020-01-21 19:48:47 +0000

    fixed undefined variable -colorpicker

--- Tuesday/colourGuessingGame/app.js
+++ Tuesday/colourGuessingGame/app.js
@@ -1,12 +1,12 @@
 const numSquares = 6;
 const colors = ["rgb(255,0,0)", "rgb(255,0,255)", "rgb(255,225,0)", "rgb(255,0,255)", "rgb(0,255,255)", "rgb(0,255,0)"];
-const pickedColor;
-// const squares = document.querySelectorAll(".square");
+//const pickedColor;
+const squares = document.querySelectorAll(".square");
 const resetButton = document.querySelector("#reset");
 const modeButtons = document.querySelectorAll(".mode");
 
-const squares = document.querySelectorAll(.squares);
+
 
 for(i=0; i<squares.length; i++) {
-    squares.style.backgroundColor = colors[i]
+    squares[i].style.backgroundColor = colors[i]
 }
